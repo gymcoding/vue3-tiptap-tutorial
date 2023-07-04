26cfd4c6e0fd516c6c75b1a695048229d71f226c
gymcoding <bruce.lean17@gmail.com>
2023-07-04 15:24:08 +0900

    1

---
+++ src/components/Tiptap.vue
@@ -0,0 +1,22 @@
+<template>
+	<editor-content :editor="editor" />
+</template>
+
+<script setup>
+import { useEditor, EditorContent } from '@tiptap/vue-3';
+import StarterKit from '@tiptap/starter-kit';
+
+const props = defineProps({
+	modelValue: {
+		type: String,
+		defalt: '',
+	},
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const editor = useEditor({
+	content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+	extensions: [StarterKit],
+});
+</script>
